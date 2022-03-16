fb49bc48012879e5ee08aad277ec5a69a6dca65d
gaganku72 <gagan.ku72@gmail.com>
2022-03-16 09:41:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/zuescoder69/wordle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zuescoder69.wordle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
